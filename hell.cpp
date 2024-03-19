5bcb1968f3cf7b65d3c4e10f4de5965d5b288daf
dhara58 <dharalunagariya5898@gmail.com>
2024-03-19 18:17:25 +0530

    first commit

---
+++ hell.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=20;
+    cout<<"enter the value:";
+    cin>>i;
+    
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+{
+ int i=50;
+  //cout <<"enter the value:";
+  //cin >>i;
+  cout <<"hello world"<< i<<endl;
+  cout <<"hello world";
+
+  return 0;
+}
