672a6ecfbbb48fd24e9a00ea00ebd3e5dec6de32
dhara58 <dharalunagariya5898@gmail.com>
2024-03-21 18:09:23 +0530

    commit

---
+++ loop3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    char a='A';
+
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<=1;j++)
+        {
+            cout<< a<<endl;
+            a++;
+        }
+            cout<<"\n";
+    }
+        return 0;
+}
